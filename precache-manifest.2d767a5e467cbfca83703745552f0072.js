39d9b622d93989d39477330763402583a2eefe36
Veselin Valev <vesselinvalev@gmail.com>
2019-02-19 23:14:02 +0200

    Updates

---
+++ precache-manifest.2d767a5e467cbfca83703745552f0072.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "23cef479eb63c628f1030477dfcf35c5",
+    "url": "/Project-Organizer/static/media/background-6.23cef479.jpg"
+  },
+  {
+    "revision": "c75d0d003fb395216d5d",
+    "url": "/Project-Organizer/static/js/runtime~main.c75d0d00.js"
+  },
+  {
+    "revision": "e406e4e7551c40f256c1",
+    "url": "/Project-Organizer/static/js/main.e406e4e7.chunk.js"
+  },
+  {
+    "revision": "6e4a37d3370b082c4e44",
+    "url": "/Project-Organizer/static/js/1.6e4a37d3.chunk.js"
+  },
+  {
+    "revision": "e406e4e7551c40f256c1",
+    "url": "/Project-Organizer/static/css/main.c99f39b1.chunk.css"
+  },
+  {
+    "revision": "c43c65ed2ca7d08e7b920540235b3a91",
+    "url": "/Project-Organizer/index.html"
+  }
+];
